d6a334558c2991247e5e322f81eebd5c63557182
ayao <59346123+a1yao@users.noreply.github.com>
2023-05-05 16:12:33 -0700

    finished explore and expose

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,50 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  let voices = [];
+
+  const textArea = document.querySelector("textarea");
+  const dropDown = document.querySelector("select");
+  const talkButton = document.querySelector("button");
+  const smiley = document.querySelector("img");
+
+  var utterance;
+
+
+  setTimeout(() => {
+    voices = synth.getVoices();
+    let voice;
+  
+    for(let i = 0; i < voices.length; i++) {
+      let newOption = new Option(voices[i].name, i);
+      dropDown.add(newOption, undefined);
+    }
+
+    textArea.addEventListener("change", (event) => {
+        utterance = new SpeechSynthesisUtterance(event.target.value);
+        utterance.voice = voice;
+    });
+
+    dropDown.addEventListener("input", (event) => {
+        voice = voices[event.target.value];
+        utterance.voice = voice;
+    })
+
+    talkButton.addEventListener("click", () => {
+        synth.speak(utterance);
+        smiley.src = "assets/images/smiling-open.png";
+    })
+
+
+    setInterval(() => {
+        if(synth.speaking == false) {
+            smiley.src = "assets/images/smiling.png";
+        }
+    }, 0);
+    
+
+}, 50);
+
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,53 @@
 // expose.js
 
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti();
+  const mainImg = document.querySelector("section > img");
+  const mainAudio = document.querySelector("audio");
+  const volumeIcon = document.querySelector("div > img");
+
+  const dropDown = document.querySelector("select");
+  const slider = document.querySelector("input");
+  const playButton = document.querySelector("button");
+
+  var sound;
+
+
+  dropDown.addEventListener("input", updateImg);
+  slider.addEventListener("input", updateVolume);
+  playButton.addEventListener("click", playSound);
+
+  function updateImg(e) {
+    mainImg.src = "assets/images/" + e.target.value + ".svg";
+    mainAudio.src = "assets/audio/" + e.target.value + ".mp3";
+    sound = e.target.value;
+  }
+
+  function updateVolume(e) {
+    mainAudio.volume = e.target.value/100;
+    if(e.target.value == 0) {
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+    }
+    else if(e.target.value < 33) {
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+    }
+    else if(e.target.value < 67) {
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+    }
+    else {
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+    }
+  }
+
+  function playSound() {
+    mainAudio.play();
+    console.log(sound);
+    if(sound == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+    
+  }
 }
